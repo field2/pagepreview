bd946069ca26928ecadc9c012ee460d6ffcc74f1
Dunkle, Ben <ben@field2.com>
2014-05-09 06:42:32 -0400

    A few minor changes

--- page-preview.php
+++ page-preview.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Page Preview by EmpireOfLight
+ * Plugin Name: Page Preview
  * Plugin URI: https://github.com/field2/Page-Preview.git
- * Description: A simple plugin to show page previews. Just add the shortcode [pagepreview pageid=x] where x is the id of your page, and it will build a nice little preview of the page, showing the page title, featured image, and everything up to the <!-- more --> tag from the page content.
+ * Description: A plugin to show page previews
  * Version: 0.1
  * Author: Ben Dunkle (@empireoflight)
  * Author URI: http://bendunkle.com
@@ -11,14 +11,14 @@
  
 function page_preview($atts,$pageid = null) {
 extract(shortcode_atts(array(
-		"pageid" => '0'
-	), $atts));
+        "pageid" => '0'
+    ), $atts));
   
 $the_query = new WP_Query( 'page_id=' . $pageid . '' );
      global $more;    
 if ( $the_query->have_posts() ) {
  
-	while ( $the_query->have_posts() ) {
+    while ( $the_query->have_posts() ) {
     $the_query->the_post();
     $more=0;
     $title=get_the_title();
